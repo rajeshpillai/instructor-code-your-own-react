62d94be7bdb23e5a2c8a951cf5c5842410954f86
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-08-29 20:42:12 +0530

    Render simple string/number and object

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 /** @jsx createElement */
 const list = (<ul className="todos">
-    <li className="todo-item">Build your own reactjs</li>
-    <li className="todo-item">Build your own expressjs</li>
+    <li className="todo-item">Build your own reactJS</li>
+    <li className="todo-item">Build your own expressJS</li>
 </ul>);
 
+/*  The equivalent JS code
+const list = createElement('ul', {className: 'todos'},
+    createElement('li', {className: 'todo-item'}, 'Build your own reactJS'),
+    createElement('li', {className: 'todo-item'}, 'Build your own expressJS'),
+);
+
+*/
+
 console.log(list);
+
+render(list, document.getElementById("root"));
