4ca8dae5ce2d53b6ab5357dff92604373a378a41
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-08-29 12:28:16 +0530

    Refactoring: Folder structure

---
+++ app.js
@@ -0,0 +1,7 @@
+/** @jsx createElement */
+const list = (<ul className="todos">
+    <li className="todo-item">Build your own reactjs</li>
+    <li className="todo-item">Build your own expressjs</li>
+</ul>);
+
+console.log(list);
