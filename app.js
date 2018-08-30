1e5693bcf86fc6538f0c50ef23b1a7e368f36697
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-08-30 12:14:06 +0530

    Rendering child elements

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+var todos= [
+        {id: 1, title: "Build your own reactJS", completed: false},
+        {id: 2, title: "Build your own expressJS", completed: false},
+        {id: 3, title: "Advaned NodeJS", completed: true},
+];
+
 /** @jsx createElement */
-const list = (<ul className="todos">
-    <li className="todo-item">Build your own reactJS</li>
-    <li className="todo-item">Build your own expressJS</li>
-</ul>);
+
+let li = todos.map(todo => {
+    return <li key={todo.id} className="todo-item">
+        {todo.title}
+    </li>
+});
+
+console.log("todos: ", li);
+
+const list = (
+    <ul style={{color: "blue"}} className="todos">
+        {li}
+    </ul>
+);
 
 /*  The equivalent JS code
 const list = createElement('ul', {className: 'todos'},
