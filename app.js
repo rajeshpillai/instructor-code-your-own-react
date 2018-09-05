3f77aa97b64e687972dcda708c3aac3427e3b942
urvashi sachdev <urvashi.sachdev@tekacademy.com>
2018-09-05 18:25:52 +0530

    doone for delete with anonymous function using list1

--- app.js
+++ app.js
@@ -1,151 +1,85 @@
- /** @jsx TinyReact.h */
+/** @jsx TinyReact.h */
 
-// var todos= [
-//         {id: 1, title: "Build your own reactJS", completed: false},
-//         {id: 2, title: "Build your own expressJS", completed: false},
-//         {id: 3, title: "Advaned NodeJS", completed: true},
-// ];
+var state = ["item1", "item2"];
 
-//
-
-// let li = todos.map(todo => {
-//     return <li key={todo.id} className="todo-item">
-//         {todo.title}
-//     </li>
-// });
-
-// console.log("todos: ", li);
-
-// const list = (
-//     <ul style={{color: "blue"}} className="todos">
-//         {li}
-//     </ul>
-// );
-
-// /*  The equivalent JS code
-// const list = createElement('ul', {className: 'todos'},
-//     createElement('li', {className: 'todo-item'}, 'Build your own reactJS'),
-//     createElement('li', {className: 'todo-item'}, 'Build your own expressJS'),
-// );
-
-// */
-
-// console.log(list);
-
-// render(list, document.getElementById("root"));
-
- //console.log(TinyReact);
-  
-  
-  //console.log("sasas");
-  
-   //console.log(TinyReact.h("ul",null,null));
-  
-//    TinyReact.render(document.getElementById("root"),<div>Hello !!</div>);
-
-//    TinyReact.render(document.getElementById("root"),<span>Hello World !!</span>);
-
- //TinyReact.render(document.getElementById("root"),<div>Hello !!</div>);
-
-   
-  
-  //console.log(TinyReact.render(document.getElementById("root"), {type:"div",props:null,children:["Hello World"]}));
-   
-
-// var list1 = (<ul test="aaa">
-//                 <li style="background:yellow" >item1</li>
-//                 <li>item2</li>
-//                 <li>test
-//                         <ul>
-//                             <li>abc</li>
-//                         </ul>
-//                     </li>
-//             </ul>);
-
-//   var list2 = (<ul test="bbb">
-//                     <li style="background:cyan">item1</li>
-//                     <li>item2</li>
-//                     <li>item3</li>
-//                     <li>test
-//                         <ul>
-//                             <li>def</li>
-//                         </ul>
-//                     </li>
-//                 </ul>);
-//   var list3 = (<ul><li>item11</li><li>item2</li><li>item3</li></ul>);
-
-//   var list4 = (<ul><li>item11</li><li>item2</li><li>item31</li></ul>);
-
-// TinyReact.render(document.getElementById("root"),list1);
-// TinyReact.render(document.getElementById("root"),list2);
-
- var state=["item1","item2","item3"];
-
-
-// // var li = state.map(item=>{
-// //     return (<li>{item}</li>)
-// // })
-
-// var list = (
-//     <ul>
-//         {state.map(item=>{
-//             return (<li>{item}</li>)
-//         })
-//         }
-//     </ul>
-// );
-
-// var view=(
-//     <div>
-//         <input type="button" value="Update" onClick={updateData}></input>
-//         {list}
-//     </div>
-// );
-
-// TinyReact.render(document.getElementById("root"),view);
-
-function updateData(){
-    console.log("call updateData.....");
-    var item = "item" + (state.length+1);
-    state.push(item);
-
-    reRender();
+function addData() {
+  console.log("call updateData.....");
+  var item = "item" + (state.length + 1);
+  state.push(item);
+  reRender();
 }
 
-function reRender(){
+function deleteData(item) {
+  //console.log(e.target.attributes);
 
-    // var li = state.map(item=>{
-    //     return (<li>{item}</li>)
-    // })
+  //var item = e.target.attributes["data-val"].nodeValue;
+  console.log(item);
+  var data = state;
+  state = state.filter(t => {
+    return t != item;
+  });
+  reRender();
+}
 
-    let list = (
-        <ul>
-            {state.map(item=>{
-                return (<li>{item}</li>)
-            })
-            }
-        </ul>
-    );
-    let view=(
-        <div>
-            <input type="button" value="Update" onClick={updateData}></input>
-            {list}
-        </div>
-    );
+function deleteData1(e) {
+  //console.log(e.target.attributes);
 
+  var item = e.target.attributes["data-val"].nodeValue;
+  console.log(item);
+  var data = state;
+  state = state.filter(t => {
+    return t != item;
+  });
+  reRender();
+}
 
+function reRender() {
+  let list = (
+    <ul>
+      {state.map(item => {
+        return (
+          <li>
+            {item}{" "}
+            <a href="#" onClick={() => deleteData(item)}>
+              X
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
 
-    TinyReact.render(document.getElementById("root"),view);
+  let list1 = (
+    <ul>
+      {state.map(item => {
+        return (
+          <li>
+            {item}
+            <a href="#" data-val={item} onClick={deleteData1}>
+              X
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+  let view = (
+    <div>
+      <input type="button" value="Add" onClick={addData} />
+      {list1}
+    </div>
+  );
+  TinyReact.render(document.getElementById("root"), view);
 }
 
 reRender();
 
 // setTimeout(function(){
-//     //TinyReact.update(document.getElementById("root"),list1,list2,1);  
+//     //TinyReact.update(document.getElementById("root"),list1,list2,1);
 //     TinyReact.render(document.getElementById("root"),list2);
-//     // setTimeout(function(){    
+//     // setTimeout(function(){
 //     //     TinyReact.render(document.getElementById("root"),list3);
-//     //     setTimeout(function(){             
+//     //     setTimeout(function(){
 //     //         TinyReact.render(document.getElementById("root"),list4);
 //     //     },2000)
 //     // },2000)
